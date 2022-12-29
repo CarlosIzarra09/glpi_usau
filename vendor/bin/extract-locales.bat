9d2ce54a9d8fc9853af8b69d9de8172e7171d667
CarlosIzarra09 <eduizarra2@hotmail.com>
2022-12-28 19:52:19 -0500

    rebase master

--- vendor/bin/extract-locales.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-setlocal DISABLEDELAYEDEXPANSION
-SET BIN_TARGET=%~dp0/../glpi-project/tools/bin/extract-locales
-SET COMPOSER_RUNTIME_BIN_DIR=%~dp0
-bash "%BIN_TARGET%" %*
--- vendor/bin/plugin-release.bat
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-setlocal DISABLEDELAYEDEXPANSION
-SET BIN_TARGET=%~dp0/../glpi-project/tools/tools/plugin-release
-SET COMPOSER_RUNTIME_BIN_DIR=%~dp0
-python "%BIN_TARGET%" %*
